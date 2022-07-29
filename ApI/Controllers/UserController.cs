d6606da5b0467fb4eb52dd3d18df4ec6546ba70c
Amin <MY_NAME@example.comaminahmed56344@gmail.com>
2022-07-29 20:57:07 +0200

    end of stage 2

---
+++ ApI/Controllers/UserController.cs
@@ -0,0 +1,40 @@
+﻿using ApI.Data;
+using ApI.Entiteis;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly DataContext context;
+
+        public UserController(DataContext _context)
+        {
+            context = _context;
+        }
+
+
+        [HttpGet]
+        public async Task <ActionResult<IEnumerable<AppUser>>> getusers()
+            {
+             var users = await context.Users.ToListAsync();
+            return Ok(users);    
+            }
+
+
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> getuser( int id)
+        {
+            var user = await context.Users.FirstOrDefaultAsync(e => e.Id == id);
+            return Ok(user);
+        }
+
+
+
+    }
+}
